468dd2fe8bc4ef0dfce0866310a828876c048c4f
T-101 <antti.rummukainen@gmail.com>
2023-10-24 12:35:42 +0300

    shorten recipe urls

--- backend/recipe/urls.py
+++ backend/recipe/urls.py
@@ -7,7 +7,7 @@ app_name = "recipe"
 urlpatterns = [
     path('', LandingPageView.as_view(), name="landing-page"),
     path('info/', InfoView.as_view(), name="info"),
-    path('recipe/<slug:slug>/', RecipeView.as_view(), name="detail"),
+    path('r/<slug:slug>/', RecipeView.as_view(), name="detail"),
     path('type/<slug:slug>/', RecipeTypeView.as_view(), name="types"),
     path('search/', SearchView.as_view(), name="search")
 ]
