ac953794955ff6ce4e63e12be212c68feb04fcee
T-101 <antti.rummukainen@gmail.com>
2023-12-12 10:36:30 +0200

    fixed image filesave. convert images to webp before save.

--- backend/recipe/models.py
+++ backend/recipe/models.py
@@ -1,13 +1,17 @@
 import os
+from io import BytesIO
 
 from django.db import models
 from django.utils.text import slugify
+from django.core.files.base import ContentFile
+
 from django_extensions.db.fields import AutoSlugField
+from PIL import Image
 
 
 def upload_picture(instance, orig_filename):
     _filename, ext = os.path.splitext(orig_filename)
-    return f"{slugify(instance.recipe_name)}{ext}"
+    return f"{slugify(instance.name)}{ext}"
 
 
 class Ingredient(models.Model):
@@ -31,6 +35,7 @@ class IngredientUnit(models.Model):
 class RecipeClass(models.Model):
     class Meta:
         verbose_name_plural = "Recipe classes"
+
     name = models.CharField(max_length=10)
     slug = AutoSlugField(populate_from="name")
     is_visible = models.BooleanField(default=True)
@@ -46,6 +51,21 @@ class Recipe(models.Model):
     instructions = models.TextField(blank=True, null=True)
     picture = models.ImageField(upload_to=upload_picture, blank=True)
     is_visible = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        if self.picture:
+            img = Image.open(self.picture)
+            # Convert the image to WebP format
+            if img.format != 'WEBP':
+                buffer = BytesIO()
+                img.save(buffer, format='WEBP')
+                self.picture.save(
+                    f"{self.picture.name.split('.')[0]}.webp",
+                    ContentFile(buffer.getvalue()),
+                    save=False
+                )
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
