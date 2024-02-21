b03db16d4c0cfa92aa51b4d833c42569ac386e0a
T-101 <antti.rummukainen@gmail.com>
2024-02-21 20:01:35 +0200

    ingredient specific ciew url config

--- backend/recipe/urls.py
+++ backend/recipe/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from recipe.views import LandingPageView, RecipeView, RecipeTypeView, SearchView, InfoView
+from recipe.views import LandingPageView, RecipeView, RecipeTypeView, SearchView, InfoView, RecipeIngredientView
 
 app_name = "recipe"
 
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', LandingPageView.as_view(), name="landing-page"),
     path('info/', InfoView.as_view(), name="info"),
     path('r/<slug:slug>/', RecipeView.as_view(), name="detail"),
+    path('i/<slug:slug>/', RecipeIngredientView.as_view(), name="ingredient-detail"),
     path('type/<slug:slug>/', RecipeTypeView.as_view(), name="types"),
     path('search/', SearchView.as_view(), name="search")
 ]
